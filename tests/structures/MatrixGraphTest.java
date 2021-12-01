e33ff9536ce91e7ab758c5912d8c643caae000f1
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-12-01 18:16:32 -0500

    Matrix test

--- tests/structures/MatrixGraphTest.java
+++ tests/structures/MatrixGraphTest.java
@@ -14,32 +14,44 @@ public class MatrixGraphTest {
 
 	MatrixGraph<Integer> graph;
 	List<Integer> v;
-	List<Integer> w;
+	List<Integer> e;
+
 
 	public MatrixGraph<Integer> graphScenary1() {
-		graph = new MatrixGraph<>();
+		graph = new MatrixGraph<Integer>();
 		v = new ArrayList<>();
+		v.add(1);
+		v.add(2);
+		v.add(3);
+		v.add(4);
 
+		graph.insert(1, 2, 0);
+		graph.insert(1, 3, 0);
+		graph.insert(2, 3, 0);
+		graph.insert(2, 4, 0);
+		graph.insert(3, 4, 0);
 
 		return graph;
 	}
 
 	public MatrixGraph<Integer> graphScenary2() {
-		graph = new MatrixGraph<>();
+		graph = new MatrixGraph<Integer>();
 		v = new ArrayList<>();
-		w = new ArrayList<>();
+		e = new ArrayList<>();
 
+		v.add(1);
+		v.add(2);
+		v.add(3);
+		v.add(4);
+
+		graph.insert(1, 2, 15);
+		graph.insert(1, 3, 11);
+		graph.insert(2, 3, 12);
+		graph.insert(2, 4, 14);
+		graph.insert(3, 4, 3);
 
 		return graph;
-	}
 
-	public int[][] setupScenary3(){
-		int[][] matrix = {
-				{0,3,4},
-				{3,0,1},
-				{4,1,0}
-		};
-		return matrix;
 	}
 
 
@@ -47,12 +59,41 @@ public class MatrixGraphTest {
 	public void testInsert() {
 		MatrixGraph<Integer> graph = graphScenary1();
 
+		v.add(5);
 
+		graph.insert(2, 5, 0);
+		graph.insert(4, 5, 0);
+
+		assertTrue(graph.contains(5));
 	}
 
 	@Test
 	public void testInsert2() {
 		MatrixGraph<Integer> graph = graphScenary2();
+		
+		v.add(5);
 
+		graph.insert(2, 5, 1);
+		graph.insert(4, 5, 2);
+
+		assertTrue(graph.contains(5));
 	}
+	
+	@Test
+	public void testFloyd() {
+		MatrixGraph<Integer> graph = graphScenary2();
+		
+		int[][] g = graph.floyd();
+		
+		assertEquals(0, g[0][0]);
+		assertEquals(11, g[0][2]);
+		assertEquals(14, g[0][3]);
+		assertEquals(0, g[1][1]);
+		assertEquals(12, g[1][2]);
+		assertEquals(14, g[1][3]);
+		assertEquals(0, g[2][2]);
+		assertEquals(3, g[2][3]);
+		assertEquals(0, g[3][3]);
+	}
+	
 }
