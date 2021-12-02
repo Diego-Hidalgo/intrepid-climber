74ac9cf9769655a14151bab5acb5b2d92f5a3735
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-12-01 21:19:14 -0500

    Little changes

--- src/model/Mountain.java
+++ src/model/Mountain.java
@@ -53,35 +53,6 @@ public class Mountain {
         Collections.sort(friends);
     }
 
-    /*
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("data_test.txt"));
-        File f = new File("output.txt");
-        if(!f.exists())
-            f.createNewFile();
-        PrintWriter pw = new PrintWriter(f);
-        Mountain m = new Mountain();
-        String line;
-        while((line = br.readLine()) != null) {
-            int N = Integer.parseInt(line.split(" ")[0]);
-            m.insertLandMarks(N);
-            int[] content;
-            for (int i = 0; i < N - 1; i++) {
-                content = m.parseStringArray(br.readLine().split(" "));
-                m.insertLandMarks(content);
-            }
-            content = m.parseStringArray(br.readLine().split(" "));
-            m.addFriends(content);
-            int a = m.calcMinEnergy();
-            pw.write(a + "\n");
-            m.clear();
-            pw.flush();
-        }
-        pw.close();
-        br.close();
-    }
-     */
-
     public int calcMinEnergy() {
         ListGraph<Integer> auxGraph = new ListGraph<>();
         int[][] m = landmarks.floyd();
