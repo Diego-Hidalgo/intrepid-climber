01e74c89124a00bfa1fecb7728498062445f2248
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-12-01 21:21:56 -0500

    Fixed some things

---
+++ tests/model/MountainTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import structures.GraphInterface;
+
+public class MountainTest {
+
+	private Mountain m;
+	private GraphInterface<Integer> landmarks;
+	private List<Integer> friends;
+
+	public void setupScenary1() {
+		m = new Mountain();
+	
+	}
+
+}
