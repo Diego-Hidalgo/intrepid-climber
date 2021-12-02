8789fb66ddde6b3734acd2d78b7174660e91ec5d
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-12-01 22:02:50 -0500

    Tests

--- tests/model/MountainTest.java
+++ tests/model/MountainTest.java
@@ -2,11 +2,13 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import structures.GraphInterface;
+import structures.adj_graph.ListGraph;
 
 public class MountainTest {
 
@@ -14,9 +16,50 @@ public class MountainTest {
 	private GraphInterface<Integer> landmarks;
 	private List<Integer> friends;
 
-	public void setupScenary1() {
+	public Mountain setupScenary1() {
 		m = new Mountain();
+		landmarks = new ListGraph<>();
+        friends = new ArrayList<>();
+        
+        int [] f = {5,2};
+        
+        int [] lm = {1,2,2};
+        int [] lm2 = {2,4,2};
+        int [] lm3 = {1,3,3};
+        int [] lm4 = {3,6,3};
+        int [] lm5 = {3,5,1};
+        
+        m.insertLandMarks(5);
+        m.insertLandMarks(lm);
+        m.insertLandMarks(lm2);
+        m.insertLandMarks(lm3);
+        m.insertLandMarks(lm4);
+        m.insertLandMarks(lm5);
+        
+        m.addFriends(f);
+        
+        return m;
+	}
 	
+	@Test
+	public void testInsertLandmark() {
+		Mountain m = setupScenary1();
+		landmarks = new ListGraph<>();
+		
+		int [] lm = {6,7,2};
+		m.insertLandMarks(lm);
+		
 	}
 
+	
+	@Test
+	public void testAddFriends() {
+		Mountain m = setupScenary1();
+		friends = new ArrayList<>();
+		
+		 int [] f = {3,6};
+		 m.addFriends(f);
+		 assertFalse(friends.contains(2));
+		 
+	}
 }
